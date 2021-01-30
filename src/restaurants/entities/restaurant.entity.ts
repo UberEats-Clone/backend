7c9b868473cad56ef610bdedc7b1555ced57b320
shhan730 <shhan730@gmail.com>
2021-01-31 07:58:31 +0900

    Add Config Settings

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,31 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
   @Field((is) => String)
+  @Column()
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   isVegan?: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownerName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
